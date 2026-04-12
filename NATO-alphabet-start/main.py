a6a10e48800456e20ad0853ffacd9e36ba460320
pavanhd360 <pavanhd872@gmail.com>
2026-04-12 13:31:14 +0530

    Fixed the logic error in generate_phonetic function

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -23,14 +23,24 @@
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 #
-# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 print(data.to_dict())
 
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+def generate_phenotic():
+    word = input("enter a word").upper()
 
-word = input("enter a word").upper()
-output_list=[phonetic_dict[letter] for letter in word if letter in phonetic_dict]
-print(output_list)
+    try:
+        output_list=[phonetic_dict[letter] for letter in word ]
+    except KeyError:
+        print("Sorry only letters in the alphabet")
+        generate_phenotic()
+    else:
+
+        print(output_list)
+generate_phenotic()
+
